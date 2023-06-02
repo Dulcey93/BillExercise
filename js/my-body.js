776515e25c740aa1f21ac0f971b14bdfdd463611
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-06-02 08:31:22 -0500

    feat(Add code): :sparkles: Define body component

---
+++ js/my-body.js
@@ -0,0 +1,12 @@
+export class myBody extends HTMLElement{
+    constructor(){
+        super();
+    }
+    async componete(){
+
+    }
+    connectCalback(){
+
+    }
+}
+customElements.define('my-body',myBody);
