a0fca7daafb305327a1e1d903428fd97dd3024c3
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-06-02 09:08:56 -0500

    feat(Add code): :sparkles: Working design

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
 import "./my-header.js";
-import "./my-body.js"
+import "./my-body.js";
--- js/my-body.js
+++ js/my-body.js
@@ -1,12 +1,21 @@
+/**
+ * todo: Arreglar esto ▼ 
+ * ? Esta una Mier.....da :)
+ * @var styles
+ */
+import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
 export class myBody extends HTMLElement{
     constructor(){
         super();
     }
-    async componete(){
-
+    async components(){
+        return await (await fetch("view/my-body.html")).text();
     }
-    connectCalback(){
-
+    connectedCallback(){
+        document.adoptedStyleSheets.push(styles);
+        this.components().then(html=>{
+            this.innerHTML = html;
+        })
     }
 }
 customElements.define('my-body',myBody);
