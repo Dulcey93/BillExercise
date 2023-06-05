64716228cd3ac1bea29f15aac9ce78d3ce799008
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-06-05 09:27:58 -0500

    feat(Add code): :sparkles: Working

--- js/my-body.js
+++ js/my-body.js
@@ -4,18 +4,38 @@
  * @var styles
  */
 import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
-export class myBody extends HTMLElement{
-    constructor(){
+export class myBody extends HTMLElement {
+    constructor() {
         super();
+        this.count = 1;
+        document.adoptedStyleSheets.push(styles);
     }
-    async components(){
+    async components() {
         return await (await fetch("view/my-body.html")).text();
     }
-    connectedCallback(){
-        document.adoptedStyleSheets.push(styles);
-        this.components().then(html=>{
+    async add(e){
+        let $ = e.target;
+        if ($.nodeName == "BUTTON") {
+            let plantilla = await (await fetch("view/my-productDetails.html")).text();
+            let render = new DOMParser().parseFromString(plantilla, "text/html")["body"];
+            render.children[0].id="product_"+this.count;
+            let buttons = render.querySelectorAll("button");
+            buttons.forEach(element => {
+                element.dataset.row = "product_"+this.count;
+            });
+            let div = document.createElement("my-product-details");
+            div.insertAdjacentElement("beforeend", render.children[0]);
+            document.querySelector("#products").insertAdjacentElement("beforeend", div);
+            this.count++;
+        }
+    }
+    connectedCallback() {
+        this.components().then(html => {
             this.innerHTML = html;
+            this.add = this.querySelector("#add").addEventListener("click", this.add.bind(this));
+         
         })
     }
 }
-customElements.define('my-body',myBody);
+customElements.define('my-body', myBody);
+
